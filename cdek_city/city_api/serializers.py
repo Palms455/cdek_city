6b739fa930eac485092704c3831bea2cea1829c6
Rustam Kayumov <rustrnb@yandex.ru>
2020-03-15 00:14:59 +0500

    Рабочая версия

--- cdek_city/city_api/serializers.py
+++ cdek_city/city_api/serializers.py
@@ -1,12 +1,16 @@
 from rest_framework import serializers
 from .models import City
-
+from collections import OrderedDict
 
 class CitySerializer(serializers.ModelSerializer):
-	region = serializers.CharField(source='region.name')
+	region = serializers.CharField()
 
 	class Meta:
 		model = City
 		fields = ('ID', 'city_name', 'region', 'country')
 		
+
+	def to_representation(self, instance):
+		result = super().to_representation(instance)
+		return OrderedDict([(key, result[key]) for key in result if result[key] not in ['0', None]])
 	
--- db_update.py
+++ db_update.py
@@ -1,44 +1,40 @@
 from db_settings import *
 from pyexcel_ods3 import get_data
 import os
-from peewee import *
-db.connect
-collect = {}
-for file in os.listdir(): 
-	if file.endswith('.ods'):
-		data = get_data(file)
-		print(f'Downloading {file}')
-		for i in data['Part 1'][1::]:
-			try:
-				regi = None
-				if i[3]:
-					try:
-						regi = city_api_region.get(name = i[3])
-					except DoesNotExist:
-						regi = city_api_region.create(name = i[3])
-					city = i[2]
-					if city.endswith('обл.'):
-						city = city.split(',')[0]
-					city_api_city.create(
-						ID = i[0],
-						city_name = city,
-						region = regi.id,
-						country = i[11]
-							)
-				else:
+
+
+
+def write_to_db():
+	db.connect
+	for file in os.listdir(): 
+		if file.endswith('.ods'):
+			data = get_data(file)
+			print(f'Downloading {file}')
+			for i in data['Part 1'][1::]:
+				try:
 					city = i[2]
 					try:
 						if city.endswith('обл.'):
 							city = city.split(',')[0]
 					except AttributeError:
 						city = i[2]
+					region_id = None
+					if i[3]:
+						try:
+							regi = city_api_region.get(name = i[3])
+						except DoesNotExist:
+							regi = city_api_region.create(name = i[3])
+						region_id = regi.id	
+						
 					city_api_city.create(
 						ID = i[0],
 						city_name = city,
-						region = None,
+						region = region_id,
 						country = i[11]
-							)
-			except IndexError:
-				print(f'{file} now is done!')
-				break
+						)
+				except IndexError:
+					print(f'{file} now is done!')
+					break
 	
+if __name__ == '__main__':
+	write_to_db()
